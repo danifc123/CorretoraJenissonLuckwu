7cd6f205854e543fb865da7885a796b46bd45717
danifc123 <danielcarmo29@gmail.com>
2025-11-05 17:22:45 -0300

    Start entities

--- CorretoraJenissonLuckwuAPI/Entities/Usuario.cs
+++ CorretoraJenissonLuckwuAPI/Entities/Usuario.cs
@@ -1,4 +1,6 @@
-﻿namespace CorretoraJenissonLuckwuAPI.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CorretoraJenissonLuckwuAPI.Entities
 {
     public class Usuario
     {
@@ -6,8 +8,24 @@
         public int Id { get; set; }
         #endregion
 
-        #region Properties
+        #region
+        public DateTime Created_at { get; set; }
+        public DateTime Updated_at { get; set; }
+        #endregion
 
+        #region Properties
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Senha { get; set; }
+
+        [Required]
+        public string Stream_user_id { get; set; }
+
+        [Phone]
+        public string Telefone { get; set; }
         #endregion
     }
 }
