73643b329381d941331c9c30c4d509179f8a4796
danifc123 <danielcarmo29@gmail.com>
2025-11-05 23:32:18 -0300

    Feat: Entitie favorite done

---
+++ CorretoraJenissonLuckwuAPI/Entities/Favorito.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CorretoraJenissonLuckwuAPI.Entities
+{
+    public class Favorito
+    {
+        #region
+        [Key]
+        public int Id { get; set; }
+        #endregion
+
+        #region Properties
+        [ForeignKey("Usuario")]
+        int Usuario_Id { get; set; }
+        [ForeignKey("Imovel")]
+        public int Imovel_Id { get; set; }
+        #endregion
+    }
+}
