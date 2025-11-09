765f4998046141638063cf666d06a90251a0043b
danifc123 <danielcarmo29@gmail.com>
2025-11-09 15:49:54 -0300

    Confgure db

---
+++ CorretoraJenissonLuckwuAPI/Context/CorretoraJenissonLuckwuDb.cs
@@ -0,0 +1,23 @@
+﻿using CorretoraJenissonLuckwuAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CorretoraJenissonLuckwuAPI.Context
+{
+    public class CorretoraJenissonLuckwuDb: DbContext
+    {
+        public CorretoraJenissonLuckwuDb(DbContextOptions<CorretoraJenissonLuckwuDb> options) : base(options)
+        {
+        }
+        public Administrador administradores { get; set; }
+        public Favorito favoritos { get; set; }
+        public ImagemImovel imagemImoveis { get; set; }
+        public Imovel imoveis { get; set; }
+        public Usuario usuarios { get; set; }
+        public DbSet<Administrador> Administradores { get; set; }
+        public DbSet<Favorito> Favoritos { get; set; }
+        public DbSet<ImagemImovel> ImagemImoveis { get; set; }
+        public DbSet<Imovel> Imoveis { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+
+    }
+}
