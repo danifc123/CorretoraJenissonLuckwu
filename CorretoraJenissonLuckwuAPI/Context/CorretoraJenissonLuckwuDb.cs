02db616eedf993338ca123c1659d4e19b4bc20de
danifc123 <danielcarmo29@gmail.com>
2025-11-09 15:51:15 -0300

    Refactore: regiosn

--- CorretoraJenissonLuckwuAPI/Context/CorretoraJenissonLuckwuDb.cs
+++ CorretoraJenissonLuckwuAPI/Context/CorretoraJenissonLuckwuDb.cs
@@ -5,19 +5,24 @@ namespace CorretoraJenissonLuckwuAPI.Context
 {
     public class CorretoraJenissonLuckwuDb: DbContext
     {
+      #region Constructors
         public CorretoraJenissonLuckwuDb(DbContextOptions<CorretoraJenissonLuckwuDb> options) : base(options)
         {
         }
+        #endregion
+        #region Properties
         public Administrador administradores { get; set; }
         public Favorito favoritos { get; set; }
         public ImagemImovel imagemImoveis { get; set; }
         public Imovel imoveis { get; set; }
         public Usuario usuarios { get; set; }
+        #endregion
+        #region DbSets
         public DbSet<Administrador> Administradores { get; set; }
         public DbSet<Favorito> Favoritos { get; set; }
         public DbSet<ImagemImovel> ImagemImoveis { get; set; }
         public DbSet<Imovel> Imoveis { get; set; }
         public DbSet<Usuario> Usuarios { get; set; }
-
+        #endregion
     }
 }
